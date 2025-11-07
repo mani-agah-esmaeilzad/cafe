f9e2b95c9647d180318d8a774738aa62071a290a
mani agah esmaeilzad <mani.agah86@gmail.com>
2025-11-07 19:49:41 +0330

    update favicon

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/maniagah/Desktop/cafe/cafe-menu-landing/app/menu/page.tsx
-import * as entry from '../../../../app/menu/page.js'
+// File: /Users/maniagah/Desktop/cafe/cafe-menu-landing/app/page.tsx
+import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../app/menu/page.js')
+type TEntry = typeof import('../../../app/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
